352c2b103a4f1f80279bc87777a79ad78b8034e6
Vivek <vivekanandareddy7677@gmail.com>
2021-08-08 22:31:47 +0530

    Terraform

--- 03-modules/ec2/ec2.tf
+++ 03-modules/ec2/ec2.tf
@@ -3,13 +3,16 @@ provider "aws" {
 }
 
 resource "aws_instance" "sample" {
-  ami                    = "ami-074df373d6bafa625"
-  instance_type          = "t3.micro"
+ ami                    = "ami-074df373d6bafa625"
+ instance_type          = "t3.micro"
   vpc_security_group_ids = [var.SG_ID]
   tags = {
     Name                  = "sample"
   }
 }
-variable "SG_ID" {
+variable "SG_ID" {}
 
+output "PRIVATE_IP" {
+  value = aws_instance.sample.*.private_ip
 }
+
--- 03-modules/main.tf
+++ 03-modules/main.tf
@@ -1,12 +1,16 @@
 module "sg" {
-  source = "./sg"
+  source                 = "./sg"
 }
 module "ec2" {
-  depends_on = [module.sg]
-  source = "./ec2"
-  SG_ID  = module.sg.SG_ID
+  depends_on             = [module.sg]
+  source                 = "./ec2"
+  SG_ID                  = module.sg.SG_ID
 }
 
 provider "aws" {
   region                 = "us-east-1"
+}
+
+output "PRIVATE_IP" {
+  value = module.ec2.PRIVATE_IP
 }
