d81a26d0d41078c0bdf075882656f2ec29d4e7e7
Vivek <vivekanandareddy7677@gmail.com>
2021-08-08 16:46:14 +0530

    Terraform

---
+++ 02-remote-state/sample.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "sample" {
+  ami = "ami-074df373d6bafa625"
+  instance_type = "t3.micro"
+  tags = {
+    Name="sample"
+  }
+}
+
+
+
+terraform {
+  backend "s3" {
+    bucket = "terraform277"
+    key    = "sample/terraform-state.tf"
+    region = "us-east-1"
+  }
+}
