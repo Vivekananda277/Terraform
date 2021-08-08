cd79dcac2c52533ffc71b9b3c59118a73313d3f9
Vivek <vivekanandareddy7677@gmail.com>
2021-08-08 22:38:49 +0530

    Terraform

--- 03-modules/ec2/ec2.tf
+++ 03-modules/ec2/ec2.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  region                 = "us-east-1"
-}
-
 resource "aws_instance" "sample" {
   ami                    = "ami-074df373d6bafa625"
   instance_type          = "t3.micro"
