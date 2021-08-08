cd8cd5cda139af8b07f06a0f868a5755cdf75d5b
Vivek <vivekanandareddy7677@gmail.com>
2021-08-08 22:37:34 +0530

    Terraform

--- 03-modules/ec2/ec2.tf
+++ 03-modules/ec2/ec2.tf
@@ -3,9 +3,11 @@ provider "aws" {
 }
 
 resource "aws_instance" "sample" {
- ami                    = "ami-074df373d6bafa625"
- instance_type          = "t3.micro"
+  ami                    = "ami-074df373d6bafa625"
+  instance_type          = "t3.micro"
   vpc_security_group_ids = [var.SG_ID]
+
+
   tags = {
     Name                  = "sample"
   }
