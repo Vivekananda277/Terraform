4c984b8b82b375c6717df24b9b9b7e7ac9427fad
Vivek <vivekanandareddy7677@gmail.com>
2021-08-08 17:44:53 +0530

    Terraform

--- 03-modules/main.tf
+++ 03-modules/main.tf
@@ -2,6 +2,11 @@ module "sg" {
   source = "./sg"
 }
 module "ec2" {
+  depends_on = [module.sg]
   source = "./ec2"
   SG_ID  = module.sg.SG_ID
+}
+
+provider "aws" {
+  region                 = "us-east-1"
 }
