cdd5c0555e0137eb07bacc713dfce5aca43a6917
Vivek <vivekanandareddy7677@gmail.com>
2021-08-09 17:18:19 +0530

    Robotshop App

--- roboshop-shell-scripting/ec2.tf
+++ roboshop-shell-scripting/ec2.tf
@@ -3,8 +3,9 @@ resource "aws_spot_instance_request" "cheap_worker" {
   ami                       = "ami-074df373d6bafa625"
   spot_price                = "0.0031"
   instance_type             = "t3.micro"
-  //wait_for_fulfillment      = true
-  //spot_type                 = "persistent"
+  wait_for_fulfillment      = true
+  spot_type                 = "persistent"
+  instance_interruption_behaviour = "stop"
   tags                      = {
     Name                    = element(var.COMPONENTS, count.index)
   }
